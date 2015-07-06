2792ffcd0fdbebcd4771aef72866ae2eb52ab071
Thirty-Ninene <student10@js-c02mxpvdg083.punahou.edu>
2015-07-06 13:39:55 -1000

    Imports?

---
+++ JavaGuide.js
@@ -0,0 +1,25 @@
+//Below are variable guides.
+
+var aloha = "Hello and Goodbye";
+console.log;
+//The above is how you make a variable.
+
+var year = prompt("What year were you born? Please write all lower case and in numbers.");
+	if (year < 2000)
+	{
+		window.alert("OOOLLLLDDDIIIIEEEE Confirmed");
+	}
+	else if (year === 2000)
+	{
+		window.alert("TEEENAAGGEERR Confirmed");
+	}
+	else if(year > 2015)
+	{
+		window.alert("LIAR!!!!!!! Or time traveler? I GOT YOU TIME TRAVELER STOP IN YOUR SEAT I'M CALLING THE GOVERMENT");
+	}
+	else
+	{
+		window.alert("Either toddler or random 11 year old confirmed");
+	}
+//The above is a 3-way input-response variable.
+
---
+++ Other.js
@@ -0,0 +1,33 @@
+var year = prompt("What year were you born?");
+	if(year < 2000)
+	{
+		window.alert("You were born before 2000");
+	}
+	else if(year == 2000)
+	{
+		window.alert("you were born in 2000");
+	}
+	else
+	{
+		window.alert("You were born after 2000");
+	}	
+
+var year = prompt("What year were you born?");
+	if(year < 2000)
+	{
+		window.alert("You were born before 2000");
+	}
+	else if(year == 2000)
+	{
+		window.alert("you were born in 2000");
+	}
+	else if(year > 2000)
+	{
+		window.alert("You were born after 2000");
+	}	
+	else
+	{
+		window.alert("INVALID");
+	}
+
+	
